d7396c2af02948c6b1a575721006cb500dba7af1
Cordelle <cordellec.charbonneau@hotmail.com>
2023-03-28 14:44:08 -0600

    fixed an error

--- src/routes/achievements/2023-03-28-supabase/+page.server.js
+++ src/routes/achievements/2023-03-28-supabase/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient.js";
 
 export async function load() {
-  const { data, error } = await supabase.from("countries").select()
+  const { data } = await supabase.from("countries").select()
   return {
 
     countries: data ?? [],
